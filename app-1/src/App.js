8be77e50689d408402ede177f948dc02cabf9139
Wheatley <Wheatley@MacBook-Pro.local>
2017-12-04 13:48:10 -0700

    woot

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -3,6 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+    super()
+
+    this.state = {
+      userInput : ''
+    }
+  }
+
+  updateInput(newString) {
+    this.setState ({
+      userInput : newString
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,8 +26,9 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <input onChange={e => this.updateInput(e.target.value)}/>
         </p>
+        <p> {this.state.userInput}</p>
       </div>
     );
   }
