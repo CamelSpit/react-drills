20b2ec02d048d034b709f8ad70be7ef1d0e283cb
Wheatley <tiarra.stout@gmail.com>
2018-01-11 13:04:18 -0700

    round3

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -3,19 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+
+    this.state={
+      input: ''
+    }
+
+    this.change = this.change.bind(this)
+  }
+
+  change(e){
+    this.setState({
+      input : e.target.value
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <input onChange={this.change}/><br/>
+        {this.state.input}
       </div>
     );
   }
 }
 
 export default App;
+
+//teypin text box and it shows up as text under the input box
